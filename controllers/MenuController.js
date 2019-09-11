b422c6d6015c9126fc360f0ae8de2df6986fc790
Adam Moran <pipboy.moran9@gmail.com>
2019-09-11 11:43:02 -0500

    checkpoint-4

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -11,7 +11,8 @@ module.exports = class MenuController {
           choices: [
             "Add new contact",
             "View all contacts",
-            "Get today's date",
+            "Get time",
+            "Search for a contact",
             "Exit"
           ]
         }
@@ -34,6 +35,12 @@ module.exports = class MenuController {
         default:
           console.log("Invalid input");
           this.main();
+        case "Search for a contact":
+          this.search();
+          break;
+        case 'Get time':
+          this.getTime();
+          break;
       }
     })
     .catch((err) => {
@@ -57,28 +64,113 @@ module.exports = class MenuController {
     });
   }
 
-  getContacts(){
-    this.clear();
+   getContactCount(){
+       console.log(this.contacts.length);
+       this.main();
+       return this.contacts.length;
+     }
 
-    this.book.getContacts().then((contacts) => {
-      for (let contact of contacts) {
-        console.log(`
-        name: ${contact.name}
-        phone number: ${contact.phone}
-        email: ${contact.email}
-        ---------------`
-        );
+    getContacts(){
+       this.clear();
+       this.book.getContacts().then((contacts) => {
+         for(let contact of contacts) {
+          this._printContact(contact);
+				}
+        this.main();
+      }).catch((err) => {
+        console.log(err);
+        this.main();
+      });
+    }
+
+    search(){
+      inquirer.prompt(this.book.searchQuestions).then((target) => {
+        this.book.search(target.name).then((contact) => {
+          if(contact === null){
+            this.clear();
+            console.log("Contact not found");
+            this.search();
+          } else {
+            this.showContact(contact);
+          }
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        this.main();
+      });
+    }
+
+    showContact(contact){
+      this._printContact(contact);
+    inquirer.prompt(this.book.showContactQuestions)
+    .then((answer) => {
+      switch(answer.selected){
+        case "Delete contact":
+          this.delete(contact);
+          break;
+        case "Main menu":
+          this.main();
+          break;
+        default:
+          console.log("Something went wrong.");
+          this.showContact(contact);
       }
-      this.main();
-    }).catch((err) => {
+    })
+    .catch((err) => {
       console.log(err);
-      this.main();
+      this.showContact(contact);
     });
-  }
+    }
+
+    _printContact(contact){
+      console.log(`
+      name: ${contact.name}
+      phone: ${contact.phone}
+      email: ${contact.email}
+      ---------------------`
+    );
+    }
+
+    delete(contact){
+      inquirer.prompt(this.book.deleteConfirmQuestions)
+      .then((answer) => {
+        if(answer.confirmation){
+          this.book.delete(contact.id);
+          console.log("contact deleted!");
+          this.main();
+        } else {
+          console.log("contact not deleted");
+          this.showContact(contact);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        this.main();
+      });
+    }
 
   exit(){
     console.log("Thanks for using AddressBloc!");
     process.exit();
   }
-
-}
+	getTime() {
+		function addZero(i) {
+			if (i < 10) {
+				i = '0' + i;
+			}
+			return i;
+		}
+		var currentDate = new Date();
+		var hours = addZero(currentDate.getHours());
+		var minutes = addZero(currentDate.getMinutes());
+		var seconds = addZero(currentDate.getSeconds());
+		var date = addZero(currentDate.getDate());
+		var month = addZero(currentDate.getMonth() + 1);
+		var year = currentDate.getFullYear();
+		var dateString = 'It\'s ' + hours + ':' + minutes + ':' + seconds + ' on ' + month + '/' + date + '/' + year;
+		console.log(dateString);
+		this.main();
+		return dateString;
+	}
+};
